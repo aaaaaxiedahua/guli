bcc2fb0e10158c400b6dab297f392c2d4bd1938c
dahua <china.123.com>
2024-11-25 20:33:31 +0800

    项目初始化

---
+++ guli_coupon/src/test/java/com/xiehuan/gulimail/guli_coupon/GuliCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiehuan.gulimail.guli_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guli_ware/src/main/java/com/xiehuan/gulimail/guli_ware/GuliWareApplication.java
@@ -0,0 +1,13 @@
+package com.xiehuan.gulimail.guli_ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliWareApplication.class, args);
+    }
+
+}
